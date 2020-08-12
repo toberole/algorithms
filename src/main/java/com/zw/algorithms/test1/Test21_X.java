1234236788a4331beb4791b7db71806316497c5c
zhouwei215664 <zhouwei215664@sogou-inc.com>
2020-08-12 10:32:21 +0800

    合并两个有序数组

--- src/main/java/com/zw/algorithms/test1/Test21_X.java
+++ src/main/java/com/zw/algorithms/test1/Test21_X.java
@@ -25,6 +25,51 @@ public class Test21_X {
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        /**
+         *  三指针:
+         *      指针一p1、nums1有效元素尾部；
+         *      指针二p2、nums2尾部；
+         *      指针三p3、最终数组尾部
+         *  1.当，p1>=0时，nums[p1],nums[p2]对比
+         *      1.1 nums[p1]大，将nums[p1]放入p3位置。p1--,p3--
+         *      1.2 nums[p2]大于等于nums[p1]，将nums[p2]放入p3位置。p2--,p3--
+         *  2.当，p1<0时，将nums[p2]放入p3位置。p2--,p3--
+         *      循环结束条件：p2<0
+         */
+
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p3 = m + n - 1;
+
+        while (p2 >= 0) {
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p3--] = nums1[p1--];
+            } else {
+                nums1[p3--] = nums2[p2--];
+            }
+        }
+    }
+
+
+    public static void merge2(int[] nums1, int m, int[] nums2, int n) {
+        // two get pointers for nums1 and nums2
+        int p1 = m - 1;
+        int p2 = n - 1;
+        // set pointer for nums1
+        int p = m + n - 1;
+
+        // while there are still elements to compare
+        while ((p1 >= 0) && (p2 >= 0))
+            // compare two elements from nums1 and nums2
+            // and add the largest one in nums1
+            nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+
+        // add missing elements from nums2
+        System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+
+    }
+
+    public static void merge1(int[] nums1, int m, int[] nums2, int n) {
         System.arraycopy(nums2, 0, nums1, m, n);
         int len = m + n;
         // 冒泡排序
