5497b260016cf5b5dee42871d7116ec3f763d6af
zhouwei215664 <zhouwei215664@sogou-inc.com>
2020-08-17 19:38:04 +0800

    note

---
+++ src/main/java/com/zw/base/test/Test3.java
@@ -0,0 +1,11 @@
+package com.zw.base.test;
+
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("Test3 ......");
+    }
+
+    public void test() {
+
+    }
+}
