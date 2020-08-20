e7fab2ba2c7ea898ce6e59547f1fee5092ad4a80
zhouwei215664 <zhouwei215664@sogou-inc.com>
2020-08-20 19:57:43 +0800

    阶乘后的零

---
+++ src/main/java/com/zw/algorithms/test1/Test35_X.java
@@ -0,0 +1,31 @@
+package com.zw.algorithms.test1;
+
+/**
+ * 阶乘后的零
+ * 给定一个整数 n，返回 n! 结果尾数中零的数量。
+ * <p>
+ * 示例 1:
+ * 输入: 3
+ * 输出: 0
+ * 解释: 3! = 6, 尾数中没有零。
+ * <p>
+ * 示例 2:
+ * 输入: 5
+ * 输出: 1
+ * 解释: 5! = 120, 尾数中有 1 个零.
+ */
+public class Test35_X {
+    public static void main(String[] args) {
+        int n = 30;
+        System.out.println(trailingZeroes(n));
+    }
+
+    public static int trailingZeroes(int n) {
+        int count = 0;
+        while(n >= 5) {
+            count += n / 5;
+            n /= 5;
+        }
+        return count;
+    }
+}
