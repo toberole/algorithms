f30127f9da4d0f6ce254ef7f4101ebb99fab7636
zhouwei215664 <zhouwei215664@sogou-inc.com>
2020-08-21 10:04:06 +0800

    旋转数组

---
+++ src/main/java/com/zw/algorithms/test1/Test36.java
@@ -0,0 +1,44 @@
+package com.zw.algorithms.test1;
+
+/**
+ * 旋转数组
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ * <p>
+ * 示例 1:
+ * 输入: [1,2,3,4,5,6,7] 和 k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ * <p>
+ * 示例 2:
+ * 输入: [-1,-100,3,99] 和 k = 2
+ * 输出: [3,99,-1,-100]
+ * 解释:
+ * 向右旋转 1 步: [99,-1,-100,3]
+ * 向右旋转 2 步: [3,99,-1,-100]
+ */
+public class Test36 {
+    public static void main(String[] args) {
+        int nums[] = {1, 2, 3, 4, 5, 6, 7};
+        rotate(nums, 3);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+
+    public static void rotate(int[] nums, int k) {
+
+    }
+
+    public static void rotate1(int[] nums, int k) {
+        for (int i = 0; i < k; i++) {
+            int temp = nums[nums.length - 1];
+            for (int j = nums.length - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = temp;
+        }
+    }
+}
