84577b5aada7279c749a7f83e078a0fab15101de
zhouwei215664 <zhouwei215664@sogou-inc.com>
2020-09-14 19:07:09 +0800

    note

---
+++ src/main/java/com/zw/base/test/Test8.java
@@ -0,0 +1,27 @@
+package com.zw.base.test;
+
+public class Test8 {
+    private static int i = 0;
+
+    public static void main(String[] args) {
+        Object object = new Object();
+        System.out.println(object);
+
+        for (int j = 0; j < 1000; j++) {
+            new Thread(() -> {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                i++;
+            }).start();
+        }
+
+        while (Thread.activeCount() > 1) {
+            Thread.yield();
+        }
+
+        System.out.println("end ...... i: " + i);
+    }
+}
