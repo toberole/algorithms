cabef597f6dfc33acc088f3c51a6a6f402a94acd
zhouwei215664 <zhouwei215664@sogou-inc.com>
2020-10-30 18:59:48 +0800

    note

--- src/main/java/com/zw/base/kotlin/Test4.kt
+++ src/main/java/com/zw/base/kotlin/Test4.kt
@@ -1,12 +1,39 @@
 package com.zw.base.kotlin
 
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
+
 open class TT4 constructor(val t: Int) {
     fun sys() {
         println(t)
     }
 
-    fun sys1(hello: (name: String) -> Unit) {
+    suspend fun sys1(hello: (name: String) -> Unit) {
+        /**
+         * coroutineScope和supervisorScope的区别在于：
+         *      coroutineScope会在任意一个协程发生异常后取消所有的子协程的运行，
+         *      而supervisorScope并不会取消其他的子协程。
+         */
+        coroutineScope {
+            launch {
+                println("1 ......")
+            }
 
+            launch {
+                println("2 ......")
+            }
+        }
+
+        supervisorScope {
+            launch {
+                println("3 ......")
+            }
+
+            launch {
+                println("4 ......")
+            }
+        }
     }
 
 
