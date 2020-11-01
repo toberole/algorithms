f96789c3309ec846344862f03041c5de55d428ec
zhouwei441300 <zhouwei441300@163.com>
2020-11-01 15:12:58 +0800

    note

--- src/main/java/com/zw/base/kotlin/Test9.kt
+++ src/main/java/com/zw/base/kotlin/Test9.kt
@@ -4,16 +4,27 @@ class HTML {
     fun body() {
         println("body ......")
     }
+
+    fun body1(i: Int) {
+        println("body1 i = $i")
+    }
 }
 
+// 带有接受者的函数字面值
 fun html(init: HTML.() -> Unit): HTML {
-    val html = HTML()  // 创建接收者对象
-    html.init()        // 将该接收者对象传给该 lambda
-    return html
+    // 创建接收者对象
+    val h = HTML()
+
+    // 将该接收者对象传给该 lambda
+    // 在lambal中就隐式的使用对象h
+    h.init()
+
+    return h
 }
 
 fun main() {
     html {       // 带接收者的 lambda 由此开始
         body()   // 调用该接收者对象的一个方法
+        body1(1)
     }
 }
