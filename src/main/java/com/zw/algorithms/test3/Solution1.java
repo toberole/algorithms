43cbd3a9df38afaeffaedc5ad9d725299db11ebe
zhouwei215664 <zhouwei215664@sogou-inc.com>
2021-06-11 16:00:45 +0800

    note

---
+++ src/main/java/com/zw/algorithms/test3/Solution1.java
@@ -0,0 +1,24 @@
+package com.zw.algorithms.test3;
+
+/**
+ * 给定不同面额的硬币和一个总金额。
+ * 写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。
+ */
+public class Solution1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        int amount = 5;
+        int coins[] = {1,2,5};
+    }
+
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+}
